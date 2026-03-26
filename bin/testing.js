f54d059511696c9eea580420f4e62375f49cd246
shekhar <shekhart442@gmail.com>
2026-03-26 20:14:20 +0530

    test

--- bin/testing.js
+++ bin/testing.js
@@ -1,11 +1,6 @@
-function calAvg(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum / arr.length;
+function addSum(a,b) {
+    return a + b;
 }
 
-const arr = [10, 20, 30, 40, 50];
-const average = calAvg(arr);
-console.log("Average:", average);
+let sum = addSum(1+1);
+console.log(sum)
